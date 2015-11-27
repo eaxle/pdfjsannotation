8f7f06e6eaf6adc483ca13f5a9192170eb2772b4
ramesh <ramesh.khadka@zurelsoft.com>
2015-11-27 11:51:21 +0545

    annotation modified

--- web/showData.js
+++ web/showData.js
@@ -33,9 +33,22 @@ function getHightlightCoords() {
 }
 //highlights the selected div with given color
 function showHighlight(selected) {
+	var container=null;
     var pageIndex = selected.page;
     var page = window.PDFViewerApplication.pdfViewer._pages[pageIndex];
-	var pageElement = page.canvas.parentNode.nextSibling;
+	var pageElement = page.canvas.parentElement;
+	if($("#pageContainer"+(pageIndex+1)).children(".customAnnotation").length==0){
+	//var pageElement = page.canvas.parentNode.nextSibling;
+	container=document.createElement('div');//this contains all the highlights.
+	container.setAttribute('style',pageElement.getAttribute('style'))
+	container.setAttribute('class','customAnnotation')
+	
+	document.getElementById('pageContainer'+(pageIndex+1)).appendChild(container)
+}
+else{
+	container=$("#pageContainer"+(pageIndex+1)).children(".customAnnotation")[0];
+}
+	//pageElement.appendChild(container);
     var viewport = page.viewport;
     var color=document.getElementById('colorPicker').value;
     selected.coords.forEach(function (rect) {
@@ -43,9 +56,18 @@ function showHighlight(selected) {
         var el = document.createElement('div');
         el.setAttribute('style', 'position: absolute; border:none; background-color:'+color+';' +
             'left:' + Math.min(bounds[0], bounds[2]) + 'px; top:' + Math.min(bounds[1], bounds[3]) + 'px;' +
-            'width:' + Math.abs(bounds[0] - bounds[2]) + 'px; height:' + Math.abs(bounds[1] - bounds[3]) + 'px;');
-        pageElement.appendChild(el);
+            'width:' + Math.abs(bounds[0] - bounds[2]) + 'px; height:' + Math.abs(bounds[1] - bounds[3]) + 'px;z-index:90;');
+        container.appendChild(el);
+        //pageElement.appendChild(el);
       
     })
 
 }
+/*
+var event = document.createEvent('CustomEvent');
+      event.initCustomEvent('pagerendered', true, true, {
+        pageNumber: 1,
+        cssTransform: true
+      });
+      document.dispatchEvent(event);
+	  */
